6bd3765e70d2c53c7c1d233a276f95226085d62a
Satoshi Ohki <roothybrid7@gmail.com>
2017-01-05 16:33:22 +0900

    Added reference link

--- .local/lib/bash/gpip.sh
+++ .local/lib/bash/gpip.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # Use a global packages in site.USER_SITE
+# see: https://hackercodex.com/guide/python-development-environment-on-mac-osx/
 gpip() {
   PIP_REQUIRE_VIRTUALENV="" pip "$@"
 }
